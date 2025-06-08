546aca9a53a8f0e280ae68f7e350b26ccad2b619
Josip Prpić <josippr@gmail.com>
2025-06-08 18:45:00 +0200

    repo cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {HeroUIProvider} from "@heroui/react";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HeroUIProvider>
+      <App />
+    </HeroUIProvider>
   </StrictMode>,
 )
