f42b102938d0f701389a507b9f17899a7b726fba
Josip Prpić <josippr@gmail.com>
2025-06-20 14:41:13 +0200

    Add light/dark theme switch button to floating header (#10)

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,8 +6,22 @@ import { Button } from "@heroui/react";
 import UserCircle from "../../assets/icons/iconsCircle";
 import SearchIcon from "../../assets/icons/search";
 import GlobeIcon from "../../assets/icons/globe";
+import SunIcon from "../../assets/icons/sun";
+import MoonIcon from "../../assets/icons/moon";
+
+import { setTheme } from "../../redux/actions/theme";
 
 function Header() {
+
+  const theme = useSelector((state) => state.theme.theme);
+  const dispatch = useDispatch();
+
+  const handleThemeChange = () => {
+    const currentTheme = typeof theme === "string" ? theme : "light";
+    const newTheme = currentTheme === "light" ? "dark" : "light";
+    dispatch(setTheme(newTheme));
+  }
+
 
   return (
     <header className="bg-transparent w-min h-16 flex items-center justify-end gap-4 absolute top-0 right-0">
@@ -20,7 +34,14 @@ function Header() {
       <Button isIconOnly aria-label="Like" variant="bordered">
         <UserCircle className="text-zinc-300" />
       </Button>
+      <Button isIconOnly aria-label="Toggle theme" variant="bordered" onClick={handleThemeChange}>
+        {theme === "light" ? (
+          <SunIcon className="text-zinc-300" />
+        ) : (
+          <MoonIcon className="text-zinc-300" />
+        )}
+      </Button>
     </header>
-)};
+  )};
 
 export default Header;
