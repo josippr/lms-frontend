0c3434131633a7fd69ac35103f8c3e41d58c8d58
Josip Prpić <josippr@gmail.com>
2025-06-23 21:24:08 +0200

    add 404 page (#8)

---
+++ src/pages/404/index.jsx
@@ -0,0 +1,9 @@
+
+export const NotFoundPage = () => {
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <h1 className="w-full max-w-sm">404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
