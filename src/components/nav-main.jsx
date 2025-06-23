cdf3228f8a6fe2b3239d3f9be97f52ffc88d906c
Josip Prpić <josippr@gmail.com>
2025-06-23 21:21:21 +0200

    add main sidebar component (#8)

---
+++ src/components/nav-main.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+import { 
+  ChevronRight,
+  LayoutDashboard,
+  CircleGauge
+} from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar"
+
+import { useTranslation } from "react-i18next";
+
+const iconMap = {
+  LayoutDashboard,
+  CircleGauge,
+};
+
+export function NavMain() {
+  const routes = useSelector((state) => state.profile.routes.mainRoutes);
+  const { t } = useTranslation();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Platform</SidebarGroupLabel>
+      <SidebarMenu>
+        {routes && routes.map((item) => {
+          const Icon = iconMap[item.icon];
+          return (
+            <SidebarMenuItem key={item.name}>
+              <SidebarMenuButton asChild tooltip={item.title}>
+                <Link to={item.route}>
+                  {Icon && <Icon className="w-5 h-5" />} 
+                  <span>{t("generalConfig.routes." + item.name)}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          );
+        })}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
---
+++ src/components/nav-user.jsx
@@ -0,0 +1,117 @@
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { setIsLoggedIn } from "@/redux/actions/general"
+import { Link, useNavigate } from "react-router-dom"
+import { useTranslation } from "react-i18next"
+import {
+  BadgeCheck,
+  Bell,
+  ChevronsUpDown,
+  CircleUserRound,
+  LogOut,
+  Settings,
+} from "lucide-react"
+
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  useSidebar,
+} from "@/components/ui/sidebar"
+
+export function NavUser() {
+  const { isMobile } = useSidebar()
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.profile.username);
+  const email = useSelector((state) => state.profile.email);
+
+  const handleLogout = () => {
+     localStorage.removeItem('token');
+     dispatch(setIsLoggedIn(false));
+     navigate('/login');
+  };
+
+  return (
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="w-full">
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground">
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage src={user.avatar} alt={user} />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-medium">{user}</span>
+                <span className="truncate text-xs">{email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+            side={isMobile ? "bottom" : "right"}
+            align="end"
+            sideOffset={4}>
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage src={user.avatar} alt={user} />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-medium">{user}</span>
+                  <span className="truncate text-xs">{email}</span>
+                </div>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <Link to="/profile" className="w-full">
+                <DropdownMenuItem>
+                  <CircleUserRound />
+                  {t("generalConfig.routes.route.profile")}
+                </DropdownMenuItem>
+              </Link>
+              <Link to="/profile" className="w-full">
+                <DropdownMenuItem>
+                  <Settings />
+                  {t("generalConfig.routes.route.settings")}
+                </DropdownMenuItem>
+              </Link>
+              <Link to="/notifications" className="w-full">
+                <DropdownMenuItem>
+                  <Bell />
+                  {t("generalConfig.routes.route.notifications")}
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={handleLogout}>
+              <LogOut />
+              {t("generalConfig.routes.route.logout")}
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  );
+}
