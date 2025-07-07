eb81eeeea5d9578395e13425ca3c4058a1205a67
Josip Prpić <josippr@gmail.com>
2025-07-07 17:57:03 +0200

    typo fix

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -1,8 +1,8 @@
-const API_BASE = import.meta.env.PROD ? '' : '/api';
+const VITE_API_BASE = import.meta.env.PROD ? '' : '/api';
 
 export const loginUser = async (emailOrUsername, password) => {
   try {
-    const response = await fetch(`${API_BASE}/users/login`, {
+    const response = await fetch(`${VITE_API_BASE}/users/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -21,7 +21,7 @@ export const loginUser = async (emailOrUsername, password) => {
 
 export const verifyToken = async (token) => {
   try {
-    const response = await fetch(`${API_BASE}/users/verify-token`, {
+    const response = await fetch(`${VITE_API_BASE}/users/verify-token`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -41,7 +41,7 @@ export const verifyToken = async (token) => {
 // fetch profile information
 export const fetchProfile = async (token) => {
   try {
-    const response = await fetch(`${API_BASE}/profile`, {
+    const response = await fetch(`${VITE_API_BASE}/profile`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -61,7 +61,7 @@ export const fetchProfile = async (token) => {
 // update profile information
 export const updateProfile = async (token, profileData) => {
   try {
-    const response = await fetch(`${API_BASE}/profile`, {
+    const response = await fetch(`${VITE_API_BASE}/profile`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -82,7 +82,7 @@ export const updateProfile = async (token, profileData) => {
 // data api
 export const fetchData = async (token) => {
   try {
-    const response = await fetch(`${API_BASE}/data`, {
+    const response = await fetch(`${VITE_API_BASE}/data`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
