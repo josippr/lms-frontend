ac617be2a8cdf69db355309c22c9af202ae3486b
Josip Prpić <josippr@gmail.com>
2025-08-03 21:52:38 +0200

    feat: adjust sidebar layout on mobile size (#8)

--- src/components/team-switcher.jsx
+++ src/components/team-switcher.jsx
@@ -8,7 +8,7 @@ export function TeamSwitcher({
   return (
     <div className="w-full">
       <div size="lg" className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground w-full flex flex-row items-center gap-2 rounded-lg px-3 py-2 text-sm font-medium text-sidebar-primary-foreground">
-        <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+        <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg ml-[-12px]">
           <GlobeLock className="size-4" />
         </div>
         <div className="grid flex-1 text-left text-sm leading-tight">
--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -234,7 +234,7 @@ function SidebarTrigger({
       data-slot="sidebar-trigger"
       variant="ghost"
       size="icon"
-      className={cn("size-7 fixed top-4 z-50", className)}
+      className={cn("size-7 fixed top-3 z-50", className)}
       onClick={(event) => {
         onClick?.(event)
         toggleSidebar()
